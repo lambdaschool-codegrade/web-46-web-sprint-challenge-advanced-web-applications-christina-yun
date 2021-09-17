45b1776428d82868c5c2232c70ff5b0bfb1787b7
christina-yun <poncheeyun@yahoo.com>
2021-09-17 11:15:47 -0700

    Finished first BubblePage test, currently working on mock

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -3,11 +3,30 @@ import MutationObserver from 'mutationobserver-shim';
 
 import { render, screen} from "@testing-library/react";
 import BubblePage from './BubblePage';
+import mockFetchColorService from './../services/fetchColorService';
+
+jest.mock('./../services/fetchColorService')
+
+const testData = [
+    {
+        code: {hex: '#8a2be2'},
+        color: "blueviolet",
+        id: 1},
+    {
+        code: {hex: '#8a2be2'},
+        color: "blueviolet",
+        id: 2}
+     ] 
 
 test("Renders without errors", ()=> {
-    
+    render(<BubblePage />)
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
+    render(<BubblePage />)
+    mockFetchColorService.mockResolvedValueOnce(testData);
+
+
+
 });
